59a565568f1d7d3939ebcaf7f4d5938abadfcc45
q-bit-updat <qbit.updat@gmail.com>
2026-07-26 15:08:20 +0530

    Created frontend Architecture

--- backend/src/services/modelResolver.js
+++ backend/src/services/modelResolver.js
@@ -1,8 +1,14 @@
-const { getConfig, loadConfig } = require('../services/configLoader')
+const { getConfig, loadConfig } = require('./configLoader')
+const { classifyPrompt } = require('./routingService')
 
-function resolveModel(model) {
+function resolveModel(model, prompt) {
     loadConfig()
     const config = getConfig()
+
+    if (model === 'auto') {
+        const tier = classifyPrompt(prompt)
+        return config.gatewayConfig.model_aliases[tier]
+    }
 
     return config.gatewayConfig.model_aliases[model]
 }
